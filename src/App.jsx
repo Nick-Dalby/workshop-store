f26cddc03f5e1f5826fb127193c65c934cd9af55
Nick Dalby <nckdlby@gmail.com>
2022-08-31 23:03:12 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Navbar, Sidebar, Footer } from './components'
+
+
+function App() {
+  return (
+    <div>
+      <h1>hello, welcome to the store</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+
+import { Auth0Provider } from '@auth0/auth0-react';
+import { CartProvider } from './context/cart_context'
+import { FilterProvider } from './context/filter_context'
+import { ProductsProvider } from './context/products_context'
+import { UserProvider } from './context/user_context'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
